0450ff38c4746f5a458bd02940f78871387b3494
RodikYermak <rodion.yermakov.sd@gmail.com>
2019-07-10 22:19:47 -0700

    First commit

---
+++ src/build/precache-manifest.556b42a175f282def3284ed37acc5fdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eec023171e1f8746864f8168bb905bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02f49c6133ff36e05da7",
+    "url": "/static/css/main.5f2a868e.chunk.css"
+  },
+  {
+    "revision": "65525d98e6835d505d2f",
+    "url": "/static/js/2.f5e6359f.chunk.js"
+  },
+  {
+    "revision": "02f49c6133ff36e05da7",
+    "url": "/static/js/main.f75dbdd1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
